f2b264cab7b26263191c96dde41de6bbbe5857cb
Miles Ranisavljevic <itstop3@gmail.com>
2016-01-29 20:18:21 -0800

    Finish JSONService using dictionaries in place of model objects.

--- abscond/JSONService.swift
+++ abscond/JSONService.swift
@@ -11,13 +11,13 @@ import Foundation
 class JSONService {
     
     //Need to change this to a Flight class return value
-    class func parseFlightSearchJSON(JSON: NSData) -> [String]? {
+    class func parseFlightSearchJSON(JSON: NSData) -> [[String: AnyObject]]? {
         do {
             guard let baseObject = try NSJSONSerialization.JSONObjectWithData(JSON, options: NSJSONReadingOptions.MutableContainers) as? [String : AnyObject] else { return nil }
             guard let legs = baseObject["legs"] as? [[String : AnyObject]], offers = baseObject["offers"] as? [[String : AnyObject]] else { return nil }
             var legsDictionary = [String : AnyObject]()
             for leg in legs {
-                guard let legId = leg["legId"] as? String, departureTimeRaw = leg["departureTimeRaw"] as? String, arrivalTimeRaw = leg["arrivalTimeRaw"] as? String, departureAirportCode = leg["departureAirportCode"] as? String, arrivalAirportCode = leg["arrivalAirportCode"] as? String, departureAirportLocation = leg["departureAirportLocation"] as? String, arrivalAirportLocation = leg["arrivalAirportLocation"] as? String, airlineName = leg["airlineName"] as? String, duration = leg["duration"] as? String else { break }
+                guard let legId = leg["legId"] as? String, departureTimeRaw = leg["departureTimeRaw"] as? String, arrivalTimeRaw = leg["arrivalTimeRaw"] as? String, departureAirportCode = leg["departureAirportCode"] as? String, arrivalAirportCode = leg["arrivalAirportCode"] as? String, departureAirportLocation = leg["departureAirportLocation"] as? String, arrivalAirportLocation = leg["arrivalAirportLocation"] as? String, airlineName = leg["airlineName"] as? String, duration = leg["duration"] as? String else { return nil }
                 var legDictionary = [String : AnyObject]()
                 legDictionary["legId"] = legId
                 legDictionary["departureTimeRaw"] = departureTimeRaw
@@ -30,10 +30,27 @@ class JSONService {
                 legDictionary["duration"] = duration
                 legsDictionary[legId] = legDictionary
             }
+            var flightOffers = [[String : AnyObject]]()
             for offer in offers {
-                print(offer)
+                guard let legIds = offer["legIds"] as? [String], averageTotalPricePerTicket = offer["averageTotalPricePerTicket"] as? [String : AnyObject], formattedPrice = averageTotalPricePerTicket["formattedPrice"] as? String, productKey = offer["productKey"] as? String, mobileShoppingKey = offer["mobileShoppingKey"] as? String, seatsRemaining = offer["seatsRemaining"] as? Int, detailsUrl = offer["detailsUrl"] as? String else { return nil }
+                var offerLegs = [String : AnyObject]()
+                for legId in legIds {
+                    offerLegs["legId"] = legsDictionary[legId]
+                }
+                var offerDictionary = [String : AnyObject]()
+                offerDictionary["legs"] = offerLegs
+                offerDictionary["formattedPrice"] = formattedPrice
+                offerDictionary["productKey"] = productKey
+                offerDictionary["mobileShoppingKey"] = mobileShoppingKey
+                offerDictionary["seatsRemaining"] = seatsRemaining
+                offerDictionary["detailsUrl"] = detailsUrl
+                flightOffers.append(offerDictionary)
+            }
+            if flightOffers.count > 0 {
+                return flightOffers
+            } else {
+                return nil
             }
         } catch { return nil }
-        return nil
     }
 }
