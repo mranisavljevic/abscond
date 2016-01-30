3c4218c015d49e873513970a0e647fee3bd18af7
Miles Ranisavljevic <itstop3@gmail.com>
2016-01-30 10:28:25 -0800

    Begin work on FlightDetailViewModel.

--- abscond/FlightDetailViewModel.swift
+++ abscond/FlightDetailViewModel.swift
@@ -14,7 +14,7 @@ class FlightDetailViewModel {
     let price: String
     let seatsRemaining: Int
     let detailsUrl: String
-    let legs: String
+    let legs: [String]
     
     
     init(flight: Flight) {
@@ -25,8 +25,30 @@ class FlightDetailViewModel {
         self.legs = FlightDetailViewModel.formatLegs(flight.legs)
     }
     
-    private class func formatLegs(legs: [Leg]) -> String {
-        return "Legs go here"
+    private class func formatLegs(legs: [Leg]) -> [String] {
+        var segmentsList = [String]()
+        for leg in legs {
+            for segment in leg.flightSegments {
+                let formatter = NSDateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+//                2016-02-07T14:35:00.000-08:00
+                let durationFormatter = NSDateFormatter()
+                durationFormatter.dateFormat = "vH'H':mm'M'"
+                let departureDate = formatter.dateFromString(segment.departureTimeRaw)
+                let arrivalDate = formatter.dateFromString(segment.arrivalTimeRaw)
+                let durationTime = durationFormatter.dateFromString(segment.duration)
+                guard let departure = departureDate, arrival = arrivalDate, duration = durationTime else { break }
+                let departureTimeString = NSDateFormatter.localizedStringFromDate(departure, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+                let arrivalTimeString = NSDateFormatter.localizedStringFromDate(arrival, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+                let durationTimeString = NSDateFormatter.localizedStringFromDate(duration, dateStyle: NSDateFormatterStyle.NoStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+                let segmentString = "\(departureTimeString) -> \(arrivalTimeString)\n\(segment.departureAirportCode) -> \(segment.arrivalAirportCode)\n\(segment.airlineName)\n\(durationTimeString)"
+                segmentsList.append(segmentString)
+            }
+        }
+        for segment in segmentsList {
+            print(segment)
+        }
+        return segmentsList
     }
     
 }
