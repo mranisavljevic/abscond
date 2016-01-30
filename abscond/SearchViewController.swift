4cb8a5888dda1c92aec0c27b8c20a649a4f51696
William Cremin <williamcremin@gmail.com>
2016-01-29 20:11:56 -0800

    Segue from SearchVC to TableVC

---
+++ abscond/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  abscond
+//
+//  Created by William Cremin on 1/29/16.
+//  Copyright © 2016 creeperspeak. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func searchButtonPressed(sender: UIButton) {
+        
+    }
+    
+    
+    
+
+}
