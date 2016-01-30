ae8782da2ebdb567be83ec12f14ffcfa75389ef1
Alberto-Vega <el_vega@live.com>
2016-01-30 08:33:47 -0800

    Function returns a tuple.

--- abscond/AppDelegate.swift
+++ abscond/AppDelegate.swift
@@ -17,9 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        RandomAirportGenerator.tenRandomAirports()
-        
+                
 //        ExpediaAPI.searchFlights("LAX", departureDate: "2016-02-05", returnDate: "2016-02-07") { (success, data) -> () in
 //            if success {
 //                if let response = data {
