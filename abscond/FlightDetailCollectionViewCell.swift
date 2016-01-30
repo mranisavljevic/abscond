27ddcb19840fccdf206ca5e576757f0ce633ffb6
Miles Ranisavljevic <itstop3@gmail.com>
2016-01-30 11:30:17 -0800

    Add custom collection view cell.

---
+++ abscond/FlightDetailCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FlightDetailCollectionViewCell.swift
+//  abscond
+//
+//  Created by Miles Ranisavljevic on 1/30/16.
+//  Copyright © 2016 creeperspeak. All rights reserved.
+//
+
+import UIKit
+
+class FlightDetailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
